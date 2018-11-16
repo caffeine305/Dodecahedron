2aef07d7b457040c19e57a731d51b8db30bfbc84
Ricardo Lázaro <caffeine305@gmail.com>
2018-11-15 23:57:54 -0600

    New spawning method
    Updated Spawning method (press to generate instead of one-by-one click-to-generate)
    
    Also, changed Textures from Bumped-Specular to Diffuse, which also improved the performance (Now you can easily reach up to 2100 Dodecahedrons!)

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spawn : MonoBehaviour {
 
     public GameObject poli;
+    public GameObject piso;
     public GameObject scoreText;
     int score;
-	// Use this for initialization
-	void Start () {
+    float speed;
+    float amplitude;
+    Vector3 posInicial;
+    Vector3 trepidar;
+    Gyroscope gyro; 
+    Quaternion posTel;
+
+    // Use this for initialization
+    void Start() {
+
+        speed = 0.0f;
+        amplitude = 2.0f;
+        posInicial = piso.transform.position;
         score = 0;
-	}
+        gyro = Input.gyro;
+        gyro.enabled = true;
+        posTel = gyro.attitude;
+    }
 
     public void SumarScore(int sumarValorScore)
     {
@@ -18,18 +34,18 @@ public class Spawn : MonoBehaviour {
         scoreText.GetComponent<TextMesh>().text = "Count:" + score;
     }
 
-    void Update () {
-        //if (Input.touchCount > 0) //subst -- 1
-        //{
-        //Touch touch = Input.GetTouch(0); //subst -- 2
-        //Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position); // subst -- 3
+    void FixedUpdate () {
 
-        //Quaternion oriented = Random.Range 
-        
+        trepidar = posInicial + new Vector3(0.0f, amplitude * Mathf.Sin(speed * Time.time), 0.0f);
 
-        if (Input.GetMouseButtonDown(0))  //borrar -- 1
+        if (Input.touchCount > 0) //subst -- 1
         {
-            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //borrer -- 2
+        Touch touch = Input.GetTouch(0); //subst -- 2
+        Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position); // subst -- 3
+        
+        //if (Input.GetMouseButtonDown(0))  //borrar -- 1
+        //{
+        //    Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //borrer -- 2
             touchPos.x = Random.Range(-93, 88);
             touchPos.y = Random.Range(-1, 90);
             touchPos.z = Random.Range(-9,94);
@@ -39,6 +55,21 @@ public class Spawn : MonoBehaviour {
                 SumarScore(1);
         }
 
+        if (Tremor() )
+        {
+            piso.transform.position = trepidar;
+        }
+
+
+    }
+
+    bool Tremor()
+    {
+        if (posTel.x > 0.8 || posTel.y > 0.8 || posTel.z > 0.8 || posTel.w > 0.8 || posTel.x < -0.8 || posTel.y < -0.8 || posTel.z < -0.8 || posTel.w < -0.8)
+
+            return true;
+
+        else return false;
     }
 }
  
